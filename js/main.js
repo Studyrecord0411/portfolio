d0291970d1b6b45608cab958923b3106976bf6ca
Studyrecord0411 <g21460800@gmail.com>
2024-07-24 12:39:19 +0900

    レスポンシブ対応の追加

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-$(function(){
-    $('#slider').slick({
+$(document).ready(function(){
+  $('#slider').slick({
       autoplay: true,
       autoplaySpeed: 2500,
-    });
+  });
+
+  $(".hamburger").click(function(){ 
+      $(".hamburger, nav, .mask").toggleClass("active");
+  });
 });
